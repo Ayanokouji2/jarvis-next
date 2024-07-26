ff864a36198c16295c4ab5311bc7efb073a13bd4
Shivam_Sharma <shivam.sharma2023@vitstudent.ac.in>
2024-07-26 23:57:57 +0530

    chore: Update npm dependencies and add "replicate" package

---
+++ src/lib/api-limit.ts
@@ -0,0 +1,67 @@
+import  {auth}  from '@clerk/nextjs/server'
+import prismadb from '@/lib/prismadb'
+
+import { MAX_FREE_TIER } from '@/constant'
+
+export const increaseApiLimit = async () => {
+    try {
+        const { userId } = await auth()
+
+        if( !userId){
+            return
+        }
+
+        const userApiLimit = await prismadb?.userApiLimit.findUnique({
+            where:{
+                userId
+            }
+        })
+
+        if( userApiLimit ){
+            await prismadb?.userApiLimit.update({
+                where:{
+                    userId
+                },
+                data:{
+                    count: userApiLimit.count + 1
+                }
+            })
+        }
+        else{
+            await prismadb?.userApiLimit.create({
+                data:{
+                    userId,
+                    count: 1
+                }
+            })
+        } 
+    } catch (error : unknown) {
+        console.error('[API_LIMIT_ERROR]', error)
+        return null;
+    }   
+}
+
+export const checkApiLimit = async () =>{
+    try {
+        const { userId } = await auth()
+
+        if( !userId){
+            return
+        }
+
+        const userApiLimit = await prismadb?.userApiLimit.findUnique({
+            where:{
+                userId
+            }
+        })
+
+        if( userApiLimit && userApiLimit.count >= MAX_FREE_TIER){
+            return false
+        }
+        
+        return true
+    } catch (error : unknown) {
+        console.error('[API_LIMIT_ERROR]', error)
+        return null;
+    }       
+}
