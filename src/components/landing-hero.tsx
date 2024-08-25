06358bee3c9e339ce12b0f8f165bb3e79d6a48ef
Shivam_Sharma <shivam.sharma2023@vitstudent.ac.in>
2024-08-25 12:21:43 +0530

    minor bug fixes

--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import {useAuth} from '@clerk/nextjs'
 import Link from 'next/link'
 import TypewriterComponent from 'typewriter-effect'
 import { Button } from './ui/button'
 
 export default function LandingHero() {
-	const {isSignedIn} = useAuth()
 	return (
 		<div className='text-white font-bold py-36 text-center space-y-5'>
 			<div className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold'>
@@ -31,7 +29,7 @@ export default function LandingHero() {
 				Create content using AI 10x faster
 			</div>
             <div>
-                <Link href={isSignedIn? '/dashboard':'/code'}>
+                <Link href={'/dashboard'}>
                     <Button variant={'premium'} className='md:text-lg p-4 md:p-6 rounded-full font-semibold'>Start Generating For Free</Button>
                 </Link>
             </div>
--- src/components/landing-navbar.tsx
+++ src/components/landing-navbar.tsx
@@ -3,7 +3,6 @@
 import Image  from 'next/image'
 import Link from 'next/link'
 import { Montserrat } from 'next/font/google'
-import { useAuth } from '@clerk/nextjs'
 
 import { cn } from '@/lib/utils'
 import {Button} from '@/components/ui/button'
@@ -14,7 +13,6 @@ const font = Montserrat({
 })
 
 export default function LandingNavbar() {
-    const { isSignedIn } = useAuth()
 
     return (
         <nav className="p-4 bg-transparent flex items-center justify-between">
@@ -25,7 +23,7 @@ export default function LandingNavbar() {
                 <h1 className={cn('text-2xl font-bold text-white',font.className)}>Jarvis II</h1>
             </Link>
             <div className="flex-items-center gap-x-2">
-                <Link href={isSignedIn?'/dashboard':'/code'}>
+                <Link href={'/dashboard'}>
                     <Button variant={'outline'} className='rounded-full'>Get Started</Button>
                 </Link>
             </div>
