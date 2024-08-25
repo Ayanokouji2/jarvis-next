68133b97c4919883416dc0f0df11994617fddb43
Shivam_Sharma <shivam.sharma2023@vitstudent.ac.in>
2024-08-25 17:43:45 +0530

    minor ui fixes

--- src/app/api/music/route.ts
+++ src/app/api/music/route.ts
@@ -36,19 +36,20 @@ export async function POST(request : Request){
 
         const freeTier = await checkApiLimit()
         const isPro = await checkSubscription()
-
+        
         if (!freeTier && !isPro) {
             return NextResponse.json({
                 error: 'Payment Required',
-
+                
             }, { status: 403 });
         }
-
+        
         const result = await replicate.run("riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05", {
             input: {
                 prompt_b : prompt
             }
         });
+        
 
         if(!isPro)
             await increaseApiLimit()
